a3b846cd323a865e4e5aa7beb67e547913e979d0
Lara-Mateo26 <mateo.lara@est.iudigital.edu.co>
2024-06-11 19:09:40 -0500

    Frontend Login

--- pages/log-in.js
+++ pages/log-in.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import styles from '../styles/login-styles.module.scss';
+import { RiUserFill, RiLockPasswordFill } from "react-icons/ri";
 
 function LoginPage() {
   return (
-    <div>
-      <h1>Log In</h1>
-      {/* Here you can add your input fields, login button, etc. */}
+    <div className={styles.wrapper}> 
+      <form className={styles['form-container']} action="">
+        <h1 className={styles.title}>Autos Colombia</h1>
+        <div className={styles["input-box"]}> 
+          <input type="text" placeholder='Usuario' required/>
+          <RiUserFill className={styles.icon}/> 
+        </div>
+        <div className={styles["input-box"]}> 
+          <input type="password" placeholder='Contraseña' required/>
+          <RiLockPasswordFill className={styles.icon}/> 
+        </div>
+        
+        <div className={styles["remember-forgot"]}> 
+          <label><input type="checkbox"/> <a2>Recuérdame</a2></label>
+          <a href="#">¿Olvidaste tu contraseña?</a>
+        </div>
+
+        <button className={styles["submit-button"]} type="submit">Inicia sesión</button>
+      </form>
     </div>
   );
 }
 
-// Do not use any layout for the login page
 LoginPage.getLayout = (page) => page;
 
 export default LoginPage;
